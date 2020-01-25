b3694976f5d4cce91ab7cf796645d377d01da213
Mickael <pezzoni.mickael@gmail.com>
2020-01-25 17:56:57 +0100

    change get doc request

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const loginRouter = require('./routes/login');
 const docsRouter = require('./routes/docs');
 const cors = require('cors');
 
+const jwt = require('express-jwt');
+
+
+/* app.use(jwt({
+    secret: 'mickapezzoni',
+    requestProperty: 'auth/signin',
+    getToken: function fromHeaderOrQuerystring(req) {
+        console.log(req.headers.authorization);
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            return req.headers.authorization.split(' ')[1];
+        } else if (req.query && req.query.token) {
+            return req.query.token;s    
+        }
+        return null;
+    }
+}).unless({path: [/auth/g]})); */
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
