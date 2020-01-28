aee105512fc8cccfb4ae093e87706156f2d65a81
mickael2407 <pezzoni.mickael@gmail.com>
2020-01-28 10:16:53 +0100

    jwt

--- index.js
+++ index.js
@@ -5,23 +5,11 @@ const categeorieRouter = require('./routes/categories');
 const loginRouter = require('./routes/login');
 const docsRouter = require('./routes/docs');
 const cors = require('cors');
+const conf = require('./config');
+const expressJwt = require('express-jwt');
 
-const jwt = require('express-jwt');
 
-
-/* app.use(jwt({
-    secret: 'mickapezzoni',
-    requestProperty: 'auth/signin',
-    getToken: function fromHeaderOrQuerystring(req) {
-        console.log(req.headers.authorization);
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            return req.headers.authorization.split(' ')[1];
-        } else if (req.query && req.query.token) {
-            return req.query.token;s    
-        }
-        return null;
-    }
-}).unless({path: [/auth/g]})); */
+app.use(expressJwt({secret: conf.jwtSecret}).unless({path: [/auth/g]}));
 
 app.use(cors());
 
